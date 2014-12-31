7c8b1697b911c5a94fc648126e66001dd8056e29
Jelle Versele <jelle.versele@internetarchitects.be>
2014-12-31 18:43:50 +0100

    Basic class definition.

---
+++ index.js
@@ -0,0 +1,67 @@
+
+/* Dependencies
+------------------------------------------------- */
+import $ from 'jquery';
+
+
+/* Default settings
+------------------------------------------------- */
+var _defaults = {
+
+};
+
+
+/* Class definition
+------------------------------------------------- */
+class Component {
+
+	constructor (el, options) {
+		this._isDestroyed = true;
+		this._settings = $.extend(true, {}, _defaults, options);
+	}
+
+	init () {
+		this._isDestroyed = false;
+	}
+
+	destroy () {
+		this._isDestroyed = true;
+	}
+
+	get isDestroyed () {
+		return this._isDestroyed;
+	}
+
+	get $el () {
+		return this._$el;
+	}
+
+	get settings () {
+		return this._settings;
+	}
+
+	_on () {
+
+	}
+
+	_off () {
+
+	}
+
+	_query () {
+
+	}
+
+	_clearQueryCache () {
+
+	}
+
+	_resolveElement () {
+
+	}
+}
+
+
+/* Export
+------------------------------------------------- */
+export Component;
